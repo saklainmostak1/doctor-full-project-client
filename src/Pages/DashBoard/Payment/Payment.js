ae542d52d116b9644d8a3bbf6fab87773e4b1d51
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-11-23 15:19:38 +0600

    push

--- src/Pages/DashBoard/Payment/Payment.js
+++ src/Pages/DashBoard/Payment/Payment.js
@@ -1,14 +1,30 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckOutForm from './CheckOutForm';
+
+
+
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+
 
 const Payment = () => {
     const booking = useLoaderData()
-    const {treatment, price, slot , appiontmentDate} = booking
+    const { treatment, price, slot, appiontmentDate } = booking
     console.log(booking);
     return (
         <div>
             <h2 className='text-3xl'>Payment for {treatment}</h2>
             <p className="text-xl">Please Pay <strong>${price}</strong> for your appointment on {appiontmentDate} at {slot}</p>
+            <div className='w-96 my-12'>
+                <Elements stripe={stripePromise}>
+                    <CheckOutForm 
+                    booking={booking}
+                    />
+                </Elements>
+            </div>
         </div>
     );
 };
